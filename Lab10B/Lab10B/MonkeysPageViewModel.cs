302bc287c507e65314214badbd898d8abfe06bb7
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2023-06-04 22:51:52 -0500

    evidencias lab10

---
+++ Lab10B/Lab10B/MonkeysPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Lab10B;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10B
+{
+    public class MonkeysPageViewModel : ViewModelBase
+    {
+        public IList<Monkey> Monkeys { get { return MonkeyData.Monkeys; } }
+
+        Monkey selectedMonkey;
+        public Monkey SelectedMonkey
+        {
+            get { return selectedMonkey; }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
+
